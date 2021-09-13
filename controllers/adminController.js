88e8d3d203d23a6c689b36268b34473221db7804
Aldycky1 <bagusaldycky@gmail.com>
2021-09-14 01:54:06 +0700

    created show detail booking (not finished yet)

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -614,13 +614,31 @@ module.exports = {
       const booking = await Booking.find()
         .populate("memberId")
         .populate("bankId");
-      console.log(booking);
       res.render("admin/booking/view_booking", {
         booking,
         title: "Staycation | Booking",
         user: req.session.user,
         page_name: "booking",
       });
-    } catch (error) {}
+    } catch (error) {
+      res.redirect("/admin/booking");
+    }
+  },
+
+  showDetailBooking: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const booking = await Booking.findOne({ _id: id })
+        .populate("memberId")
+        .populate("bankId");
+      res.render("admin/booking/show_detail_booking", {
+        title: "Staycation | Detail Booking",
+        user: req.session.user,
+        booking,
+        page_name: "booking",
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
